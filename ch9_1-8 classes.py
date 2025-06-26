1e35f85615f9c0201fb0af50bd6b6c421a03c639
michailedf <michail.nudel@edfenergy.com>
2025-06-26 16:57:55 +0100

    Sovled problems 9-6 to 9-8

--- ch9_1-8 classes.py
+++ ch9_1-8 classes.py
@@ -68,4 +68,55 @@ new_user.increment_login_attempts()
 new_user.increment_login_attempts()
 # new_user.reset_login_attempts()
 print(new_user.login_attempts)
+
+
+class IceCreamStand(Restaurant):
+    """Ch9, Ex6. Ice cream stand"""
+    def __init__(self, name, cuisine_type):
+        super().__init__(name, cuisine_type)
+        self.flavours=['Vanilla']
+
+    def show_flavours(self):
+        for flavour in self.flavours:
+            print(flavour)
+
+
+ice_shop = IceCreamStand("Ice Cream Shop", "International")
+ice_shop.flavours=['Strawberry', 'Chocolate']
+ice_shop.describe_restaurant()
+ice_shop.show_flavours()
+
+
+class Priveleges:
+    """Ch9. Ex8. Priveleges"""
+    def __init__(self):
+        self.priveleges=["can read"]
+
+    def show_priveleges(self):
+        for privelege in self.priveleges:
+            print(privelege)
+
+# my_privelege = Priveleges()
+# my_privelege.priveleges=["can write"]
+# my_privelege.show_priveleges()
+
+# important thing to note, if a parent class is defined below the child class, it won't work!
+class Admin(User):
+    """Ch9. Ex7. Ex8  Admin"""
+    def __init__(self, first_name, last_name, age, dob):
+        super().__init__(first_name, last_name, age, dob)
+        # self.priveleges=[]
+        self.priveleges=Priveleges()
         
+
+    def show_priveleges(self):
+        for privelege in self.priveleges.priveleges:
+            print(privelege)
+
+admin_user = Admin("John", "Bishop", 55, 1970)
+admin_user.describe_user()
+# admin_user - is the instance of the admin class
+# priveleges - is the attribute of the admin_user, which is also an instance of the Priveleges class
+# priveleges - is the attribute of the Priveleges class
+admin_user.priveleges.priveleges=["can read", "can write"]
+admin_user.show_priveleges()
